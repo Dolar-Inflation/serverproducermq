3967f4a7179876d5ce0dbf48b13dd0fdf0118d1c
Dolar-Inflation <vandarkholm09@gmail.com>
2025-11-04 10:55:36 +0300

    test

---
+++ src/main/java/frdmplayer/DTO/EmployeeDTO.java
@@ -0,0 +1,16 @@
+package frdmplayer.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDTO {
+    private Integer id;
+    private String fio;
+    private String address;
+    private String position;
+}
+//test

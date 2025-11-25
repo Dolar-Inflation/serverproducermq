325f21496d17843998dc0721981b588961dd6620
Dolar-Inflation <vandarkholm09@gmail.com>
2025-11-25 17:03:09 +0300

    добавил миграцию бд а также проперти отдельно для создания бд с hibernate и миграции(Flyway) с уже некоторыми данными а также написал контейнер для версии с миграционной бд(не работает)

---
+++ src/main/resources/db/migration/V2__seed_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO employee (fio, address, position) VALUES
+    ('Иванов И.И.', 'ул. Ленина, 1', 'Менеджер'),
+    ('Петров П.П.', 'ул. Советская, 2', 'Разработчик'),
+    ('Сидоров С.С.', 'ул. Победы, 3', 'Аналитик');
+
+-- Добавим телефоны
+INSERT INTO phone (number, type) VALUES
+    ('375291112233', 'mobile'),
+    ('375172223344', 'office'),
+    ('375293334455', 'home');
+
+
+INSERT INTO employeephonerelation (employee_id, phone_id) VALUES
+    (1, 1),
+    (1, 2),
+    (2, 3);

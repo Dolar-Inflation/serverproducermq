7cd690e6abcb22b865f155ea414c56cce93e2b6c
Dolar-Inflation <vandarkholm09@gmail.com>
2026-03-12 16:42:17 +0300

    просто дописал реализацию маппера

--- Consumer/src/main/java/com/messenger/consumer/DALBuisnessLogic/Consumer/Utility/Mapper/PhoneMapper.java
+++ Consumer/src/main/java/com/messenger/consumer/DALBuisnessLogic/Consumer/Utility/Mapper/PhoneMapper.java
@@ -1,13 +1,25 @@
 package com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.Mapper;
 
+import com.messenger.consumer.DALBuisnessLogic.Consumer.DAO.Entity.Employee;
 import com.messenger.consumer.DALBuisnessLogic.Consumer.DAO.Entity.Phone;
+import com.messenger.consumer.DALBuisnessLogic.Consumer.DTO.EmployeeDTO;
 import com.messenger.consumer.DALBuisnessLogic.Consumer.DTO.PhoneNumberDTO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface PhoneMapper {
     PhoneMapper INSTANCE = Mappers.getMapper(PhoneMapper.class);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updatePhoneFromDto(PhoneNumberDTO dto, @MappingTarget Phone entity);
+
+    List<PhoneNumberDTO> mapPhoneNumberDTOList(List<Phone> phoneList);
 
     PhoneNumberDTO PhoneToPhoneNumberDTO(Phone phone);
 
--- Consumer/src/main/java/com/messenger/consumer/DALBuisnessLogic/Consumer/Utility/Strategies/Consumer/PhoneDTOConsumerStrategy.java
+++ Consumer/src/main/java/com/messenger/consumer/DALBuisnessLogic/Consumer/Utility/Strategies/Consumer/PhoneDTOConsumerStrategy.java
@@ -1,90 +1,108 @@
-//package com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.Strategies.Consumer;
-//
-//
-//import com.messenger.consumer.DALBuisnessLogic.Consumer.DTO.PhoneNumberDTO;
-//import com.messenger.consumer.DALBuisnessLogic.Consumer.Interfaces.KafkaConsumerStrategy;
-//import com.messenger.consumer.DALBuisnessLogic.Consumer.KafkaMethods.MethodsKafka;
-//import com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.CrudTemplate.PhoneCrudTemplate;
-//import com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.LockTable.LockTableService;
-//import jakarta.persistence.EntityNotFoundException;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class PhoneDTOConsumerStrategy implements KafkaConsumerStrategy {
-//
-//
-//
-//
-//
-//    private final PhoneCrudTemplate phoneCrudTemplate;
-//    private final LockTableService lockTableService;
-//
-//
-//    @Override
-//    public void handle(Object obj, MethodsKafka methodsKafka) {
-//        switch (methodsKafka) {
-//            case CREATE ->{
-//                lockTableService.lockTable("phone");
-//                try {
-//
-//
-//                    phoneCrudTemplate.create((PhoneNumberDTO) obj);
-//                } finally {
-//                    lockTableService.unlockTable("phone");
-//                }
-//            }
-//            case DELETE ->{
-//                lockTableService.lockTable("phone");
-//                try {
-//                    phoneCrudTemplate.deleteById(((PhoneNumberDTO) obj).getId());
-//                    lockTableService.unlockTable("phone");
-//                }
-//                catch(EntityNotFoundException e) {
-//                    System.err.println("Ошибка: " + e.getMessage());
-//                }
-//            }
-//            case READALL -> {
+package com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.Strategies.Consumer;
+
+
+import com.messenger.consumer.DALBuisnessLogic.Consumer.DAO.Entity.Phone;
+import com.messenger.consumer.DALBuisnessLogic.Consumer.DAO.Repository.PhoneRepository;
+import com.messenger.consumer.DALBuisnessLogic.Consumer.DTO.PhoneNumberDTO;
+import com.messenger.consumer.DALBuisnessLogic.Consumer.Interfaces.KafkaConsumerStrategy;
+import com.messenger.consumer.DALBuisnessLogic.Consumer.KafkaMethods.MethodsKafka;
+
+import com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.LockTable.LockTableService;
+import com.messenger.consumer.DALBuisnessLogic.Consumer.Utility.Mapper.PhoneMapper;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class PhoneDTOConsumerStrategy implements KafkaConsumerStrategy {
+
+
+
+
+
+    private final PhoneMapper phoneMapper;
+    private final LockTableService lockTableService;
+    private final PhoneRepository phoneRepository;
+
+
+    @Override
+    public void handle(Object obj, MethodsKafka methodsKafka) {
+        switch (methodsKafka) {
+            case CREATE ->{
+                lockTableService.lockTable("phone");
+                try {
+                    PhoneNumberDTO phoneNumberDTO = (PhoneNumberDTO) obj;
+                    Phone phone = phoneMapper.PhoneDTOToPhone(phoneNumberDTO);
+                    phoneRepository.save(phone);
+
+                } finally {
+                    lockTableService.unlockTable("phone");
+                }
+            }
+            case DELETE ->{
+                lockTableService.lockTable("phone");
+                try {
+                    PhoneNumberDTO phoneNumberDTO = (PhoneNumberDTO) obj;
+                    Phone phone = phoneMapper.PhoneDTOToPhone(phoneNumberDTO);
+                    phoneRepository.deleteById(phone.getId());
+
+                    lockTableService.unlockTable("phone");
+                }
+                catch(EntityNotFoundException e) {
+                    System.err.println("Ошибка: " + e.getMessage());
+                }
+            }
+            case READALL -> {
+                List<Phone> phoneList = phoneRepository.findAll();
+                phoneMapper.mapPhoneNumberDTOList(phoneList);
+                phoneList.forEach(System.out::println);
+
+
+
 //                List<PhoneNumberDTO> phoneDTOList = phoneCrudTemplate.readAll();
 //                phoneDTOList.forEach(System.out::println);
-//
-//            }
-//            case PATCH -> {
-//                lockTableService.lockTable("phone");
-//                try {
-//                    PhoneNumberDTO phoneDTO = (PhoneNumberDTO) obj;
+
+            }
+            case PATCH -> {
+                lockTableService.lockTable("phone");
+                try {
+                    PhoneNumberDTO phoneDTO = (PhoneNumberDTO) obj;
+                    Phone phone = phoneRepository.findById(phoneDTO.getId()).get();
+                    phoneMapper.updatePhoneFromDto(phoneDTO, phone);
+                    phoneRepository.save(phone);
 //                    phoneCrudTemplate.patch(phoneDTO.getId(), phoneDTO);
-//                    lockTableService.unlockTable("phone");
-//                }
-//                catch(EntityNotFoundException e) {
-//                    System.err.println("<UNK>: " + e.getMessage());
-//                }
-//            }
-//
-//            case READ -> {
-//                try {
-//
-//
-//                    PhoneNumberDTO dto = (PhoneNumberDTO) obj;
-//                    PhoneNumberDTO found = phoneCrudTemplate.readById(dto.getId());
-//                    System.out.println("Из базы: " + found);
-//                }
-//                catch (EntityNotFoundException e) {
-//                    System.err.println("Ошибка: " + e.getMessage());
-//                }
-//
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public String getClassName() {
-//        return PhoneNumberDTO.class.getSimpleName();
-//    }
-//
-//
-//}
-//
+                    lockTableService.unlockTable("phone");
+                }
+                catch(EntityNotFoundException e) {
+                    System.err.println("<UNK>: " + e.getMessage());
+                }
+            }
+
+            case READ -> {
+                try {
+
+
+                    PhoneNumberDTO dto = (PhoneNumberDTO) obj;
+                    Phone phone = phoneRepository.findById(dto.getId()).orElseThrow(EntityNotFoundException::new);
+                    phoneMapper.PhoneToPhoneNumberDTO(phone);
+                    System.out.println("Из базы: " + phone);
+                }
+                catch (EntityNotFoundException e) {
+                    System.err.println("Ошибка: " + e.getMessage());
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public String getClassName() {
+        return PhoneNumberDTO.class.getSimpleName();
+    }
+
+
+}
+
